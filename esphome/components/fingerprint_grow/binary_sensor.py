ac6cb41e8fa5926f9d18c38503d9ea05f8abe5b5
ShadyMCShadesen <officalatrix@gmail.com>
2024-10-05 20:20:01 +0200

    Update binary_sensor.py

--- esphome/components/fingerprint_grow/binary_sensor.py
+++ esphome/components/fingerprint_grow/binary_sensor.py
@@ -14,7 +14,7 @@ CONFIG_SCHEMA = cv.Schema(
         ),
         cv.Optional("sensing_pin"): binary_sensor.binary_sensor_schema(
             icon="mdi:fingerprint",
-            internal=true,
+            internal='true',
         ),
     }
 )
